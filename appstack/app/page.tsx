9ccfaa20be25e1cf16980f2fed019c2cd8ade0cb
GAURAV KADAM <gskadam3b@gmail.com>
2026-01-10 23:16:34 +0530

    Update page.tsx

--- appstack/app/page.tsx
+++ appstack/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            or the{" "}
-            
-            center.
+
+
+
+
           </p>
         </div>
       </main>
