575b3647af0a3ff79ed6bc9682919e4335fa2959
GAURAV KADAM <gskadam3b@gmail.com>
2026-01-10 23:14:57 +0530

    Update page.tsx

--- appstack/app/page.tsx
+++ appstack/app/page.tsx
@@ -11,12 +11,7 @@ export default function Home() {
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
+            
             center.
           </p>
         </div>
